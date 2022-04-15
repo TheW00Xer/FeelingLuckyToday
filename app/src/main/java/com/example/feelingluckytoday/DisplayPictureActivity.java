9e448bb3c88b48805599373374f5a821becd5ff3
TheW00Xer <thew00xer@gmail.com>
2022-04-15 14:59:52 +0900

    Implementation of shared storage system. App is using pictures that are already saved in device's memory.
    App displays pictures from device memory.
    Tried to add function that compares file extensions to predefined list of two usable extensions and app displays only files that match this filter. Unsuccessfully. App keeps showing black screen and Toast Message as if there were no files in directory.
    Code is commented out, instead code with only .jpg extension filter is used.
    This code shows black screen whenever there is sub-folder within the Pictures directory as it tries to access it and files within but unsuccessfully.
    
    Same filter was implemented to DisplayVideoActivity checking for .mp4 extension. This filter works well, app even compares if there are videos in custom directory Called "Lucky" and if there is nothing it switches directory to default "Movies" directory.
    
    Storage permission dialog window added.
    When user starts app the first time and pushes picture or video button a dialog window with explanation is shown first and if user selects "OK" then Android dialog window to grand Storage access permission is displayed. If user selects "Cancel" dialog window is closed and app doesn't do anything.
    
    Handling of change of state (Portrait/landscape rotation) is added. App does not refresh and restarts activity on rotation, instead uses onSavedInstanceState. However videoView does not appear centered even though boundaries for layout were defined to appear centered.

--- app/src/main/java/com/example/feelingluckytoday/DisplayPictureActivity.java
+++ app/src/main/java/com/example/feelingluckytoday/DisplayPictureActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.FileFilter;
+import java.util.Objects;
 import java.util.Random;
 
 public class DisplayPictureActivity extends AppCompatActivity {
@@ -21,46 +23,53 @@ public class DisplayPictureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_picture);
 
-        File picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        //Declaring directory where app obtains picture files
-
-        final String[] okFileExtension = new String[]{"jpg","png"};
-        //Declaring value of file name extensions we want the app to use
-
-        File[] listFiles = picturesDirectory.listFiles(new FilenameFilter() { //Creating list of files from picturesDirectory that will match our Filename Filter conditions
-            public boolean accept(File picturesDirectory, String name) { //Function that compares filenames to our Filter conditions
-                for (String extension : okFileExtension) { //String value of file extensions will be compared with extensions we declared above in okFileExtension
-                    if (name.endsWith(extension))
-                        return true; //Returns only files ending with .jpg or .png
-                }
-                return false;
-            }
-        });
-        /*
-          App can run even without this filter but if user for example has multiple folders within the Picture
-          folder, App displays blank/black screen when it attempts to get the folder.
-        */
-
+        boolean fileFound = false;
         Random randomNumber = new Random();
-        //generating random number
-        File randomPicture = null;
-        //declaring file "randomPicture"
-        if (listFiles != null) {
-            randomPicture = listFiles[randomNumber.nextInt(listFiles.length)];
-            //using randomNumber to select a video from previously obtained list of files that don't have nul value
-        }
-        Uri pictureUri = Uri.fromFile(randomPicture);
-        //getting pat to selected random picture
-
+        //Generating random number
         ImageView image = findViewById(R.id.imageView);
         //Declaring View called image, finding it by it's Id
-        image.setImageURI(pictureUri);
-        //sets path for picture to be displayed in ImageView
+        //final String[] okFileExtension = {".jpg",".png"};
+        //Declaring value of file name extensions we want the app to use
+        File picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //File picturesDirectory = new File(Environment.getExternalStorageDirectory().toString() + "/Pictures/");
+        //Declaring directory where app obtains picture files
+        try {
+            if (picturesDirectory.isDirectory()) {
+                String[] paths = picturesDirectory.list();
+                assert paths != null;
+                for (String path : paths) {
+                    //if (path.toLowerCase().endsWith(Arrays.toString(okFileExtension))) {
+                    if (path.toLowerCase().endsWith(".jpg")) {
+                        System.out.println(path);
+                        fileFound = true;
+                    } else if (!fileFound) {
+                        Toast.makeText(getApplicationContext(), "No files with .jpg or .png extension in 'Pictures' directory.", Toast.LENGTH_SHORT).show();
+                        System.out.println("No files with .jpg or .png extension in 'Pictures' directory.");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        // calling the action bar
+        if (fileFound) {
+            File[] listFiles = picturesDirectory.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+                    return !file.isHidden();
+                }
+            });
+            //Creating list of files from picturesDirectory that will match our Filename Filter conditions
+            File randomPicture = Objects.requireNonNull(listFiles)[randomNumber.nextInt(listFiles.length)];
+            Uri pictureUri = Uri.fromFile(randomPicture);
+            //Getting pat to selected random picture
+            image.setImageURI(Uri.parse(String.valueOf(pictureUri)));
+            //Sets path for picture to be displayed in imageView
+        }
+
         ActionBar actionBar = getSupportActionBar();
-        // showing the back button in action bar
-        if (actionBar != null) {
+        //Calling the action bar
+        if (actionBar != null) { //Showing the back button in action bar
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
--- app/src/main/java/com/example/feelingluckytoday/DisplayVideoActivity.java
+++ app/src/main/java/com/example/feelingluckytoday/DisplayVideoActivity.java
@@ -1,71 +1,124 @@
 package com.example.feelingluckytoday;
 
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.FileFilter;
 import java.util.Objects;
 import java.util.Random;
 
 public class DisplayVideoActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_video);
 
-        File videosDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
-        //declaring directory where app obtains video files
-
-        File[] listFiles = videosDirectory.listFiles(new FilenameFilter() { //Creating list of files from videosDirectory that will match our Filename filter conditions
-            public boolean accept(File picturesDirectory, String name) {
-                return name.endsWith(".mp4"); //Returns only files ending with .mp4
-            }
-        });
-
-        //File[] listFiles = videosDirectory.listFiles();
-        //getting list of all files in video directory
-
+        boolean fileFound = false;
         Random randomNumber = new Random();
-        //generating random number
-        File randomVideo = Objects.requireNonNull(listFiles)[randomNumber.nextInt(listFiles.length)];
-        //declaring file "randomVideo" that is required to be NonNull object and then using randomNumber to select a video from previously obtained list of files
-        Uri videoUri = Uri.fromFile(randomVideo);
-        //getting path to selected random video
-
-        // calling the action bar
-        ActionBar actionBar = getSupportActionBar();
-        // showing the back button in action bar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
+        //Generating random number
+        VideoView video = findViewById(R.id.videoView);
+        //Declaring View called video, finding it by it's Id
+        MediaController mediaController = new MediaController(this);
+        //Declaring MediaController element
+        File videosDirectory = null;
+        //Declaring final directory that will be assigned a path from either main or secondary directory after passing filters
+        File videosDirectory1 = new File(Environment.getExternalStorageDirectory().toString() + "/Lucky/");
+        //Declaring primary directory where app obtains video files
+        File videosDirectory2 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+        //Declaring secondary directory where app obtains video files if primary is not available
+        try {
+            if (videosDirectory1.isDirectory()) {
+                String[] paths = videosDirectory1.list();
+                assert paths != null;
+                for (String path : paths) {
+                    if (path.toLowerCase().endsWith(".mp4")) {
+                        System.out.println(path);
+                        fileFound = true;
+                        videosDirectory = videosDirectory1;
+                    } else if (!fileFound) {
+                        Toast.makeText(getApplicationContext(),"No files with .mp4 extension in 'Lucky' directory.", Toast.LENGTH_SHORT).show();
+                        System.out.println("No files with .mp4 extension in 'Lucky' directory");
+                        videosDirectory = videosDirectory2;
+                    }
+                }
+            } else if (videosDirectory2.isDirectory()) {
+                //assert videosDirectory != null;
+                String[] paths = videosDirectory2.list();
+                assert paths != null;
+                for (String path : paths) {
+                    if (path.toLowerCase().endsWith(".mp4")) {
+                        System.out.println(path);
+                        fileFound = true;
+                        videosDirectory = videosDirectory2;
+                    }
+                }
+                if (!fileFound) {
+                    Toast.makeText(getApplicationContext(), "No files with .mp4 extension in 'Movies' directory.", Toast.LENGTH_SHORT).show();
+                    System.out.println("No files with .mp4 extension in 'Movies' directory");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        MediaController mediaController = new MediaController(this);
-        VideoView video = findViewById(R.id.videoView);
-        video.setVideoURI(Uri.parse(String.valueOf(videoUri)));
-        //setting videoView path for video file
+        try {
+            if (fileFound) {
+                File[] listFiles = videosDirectory.listFiles(new FileFilter() {
+                    @Override
+                    public boolean accept(File file) {
+                        return !file.isHidden();
+                    }
+                });
+                //Creating list of files from videosDirectory that will match our Filename Filter conditions
+                File randomVideo = Objects.requireNonNull(listFiles)[randomNumber.nextInt(listFiles.length)];
+                Uri videoUri = Uri.fromFile(randomVideo);
+                //Getting pat to selected random video
+                video.setVideoURI(Uri.parse(String.valueOf(videoUri)));
+                //Sets path for video to be displayed in videoView
+            }
+        }   catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        //Calling the action bar
+        if (actionBar != null) { //Showing the back button in action bar
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
-                //attaches mediaController to videoView
                 mediaController.setAnchorView(video);
-                //allows mediaController to control videoView (play, pause,...)
+                //Attaches mediaController to videoView
                 video.setMediaController(mediaController);
+                //Allows mediaController to control videoView (play, pause,...)
                 video.start();
             }
         });
     }
+
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -74,4 +127,22 @@ public class DisplayVideoActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConfigurationChanged(@NotNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+            findViewById(R.id.videoView).setLayoutParams(new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+            findViewById(R.id.videoView).setLayoutParams(new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+        }
+    }
+
 }
